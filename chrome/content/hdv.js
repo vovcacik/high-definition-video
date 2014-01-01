35922e844cf2496b00acf6789ea1141076ba8fe0
Vlastimil Ovčáčík <vovcacik@github.ovcacik.org>
2014-01-01 22:19:10 +0100

    Fix #3: dont select higher quality than is necessary to get best playback a screen is capable of.

--- chrome/content/hdv.js
+++ chrome/content/hdv.js
@@ -7,6 +7,47 @@ function onYouTubePlayerReady(player){
     if (!player) return;
 
     var YT_PLAYERSTATE_UNSTARTED = -1;
+    var pixelsToQuality = {
+        // Used in portrait mode.
+        "width": [
+            // Following values are computed from height array below in 16:10 ratio,
+            // which will work perfectly for 16:10 and 16:9, but could result in
+            // smaller than optimum quality for 4:3 screens. This trade off prevents
+            // selecting higher than optimum quality on 16:10 and 16:9 screens.
+            [384, "small"],
+            [576, "medium"],
+            [768, "large"],
+            [1152, "hd720"],
+            [1728, "hd1080"],
+            [Number.POSITIVE_INFINITY, "highres"]
+        ],
+        // Used in landscape mode.
+        "height": [
+            [240, "small"],
+            [360, "medium"],
+            [480, "large"],
+            [720, "hd720"],
+            [1080, "hd1080"],
+            [Number.POSITIVE_INFINITY, "highres"]
+        ]
+    };
+
+    /**
+     * Returns optimum quality, which is the smallest video resolution that overflows
+     * the screen resolution at least on one axis. This will result in downscaling
+     * even in fullscreen mode and thus highest quality on given screen.
+     * Note: screen.height and screen.width properties return incorrect values for
+     * zoomed and unzoomed pages.
+     */
+    var getOptimumQuality = function() {
+        // Detect portrait/landscape mode.
+        var axis = screen.height <= screen.width ? "height" : "width";
+        for (var i = 0; i < pixelsToQuality[axis].length; i++) {
+            if (screen[axis] <= pixelsToQuality[axis][i][0])
+                return pixelsToQuality[axis][i][1];
+        }
+    };
+
     var setHighestQuality = function(player) {
         // We need to be little bit stubborn about setting playback quality, because:
         // a) half the time between onYouTubePlayerReady call and actual playback
@@ -17,9 +58,9 @@ function onYouTubePlayerReady(player){
         // c) changing video may result in video ad (see b).
         var intervalId = window.setInterval(function() {
             try {
-                var highestQuality = player.getAvailableQualityLevels()[0] || "highres";
-                player.setPlaybackQuality(highestQuality);
-                if (player.getPlaybackQuality() === highestQuality) {
+                var quality = getOptimumQuality();
+                player.setPlaybackQuality(quality);
+                if (player.getPlaybackQuality() === quality) {
                     window.clearInterval(intervalId);
                 }
             } catch(e) {
